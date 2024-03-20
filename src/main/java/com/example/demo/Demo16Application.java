e264b83340835ba814ea9ade8a99df68fa172f80
ivoronline <ivoronline@gmail..com>
2024-03-20 10:27:54 +0100

    Commit

--- src/main/java/com/example/demo/Demo16Application.java
+++ src/main/java/com/example/demo/Demo16Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo16Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo16Application.class, args); //Test123
+        SpringApplication.run(Demo16Application.class, args); //Test12345
     }
 
 }
